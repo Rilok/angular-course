b34843fb023bca4e1712cbef9a707c963a3f40be
Rilok <szymonskic@gmail.com>
2020-10-14 11:02:20 +0200

    make template components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {ShoppingListComponent} from './shopping_list/shoppingList.component';
+import {ShoppingListEditComponent} from './shopping_list/shopping_list_edit/shoppingListEdit.component';
+import {RecipeDetailComponent} from './recipe_book/recipe_detail/recipeDetail.component';
+import {RecipeItemComponent} from './recipe_book/recipe_item/recipeItem.component';
+import {RecipeListComponent} from './recipe_book/recipe_list/recipeList.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recipe_book/recipe_detail/recipeDetail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipeDetail.component.html',
+  styleUrls: ['./recipeDetail.component.css']
+})
+export class RecipeDetailComponent{
+
+}
